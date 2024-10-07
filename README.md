80572269d0132d48a18554dfca5fce212ffbfe79
Martin Boudreau <mboudrea@cstj.qc.ca>
2024-10-07 06:25:58 -0400

    init

---
+++ README.md
@@ -0,0 +1,23 @@
+# Application de base (app-base)
+
+Application de base pour débuter un projet
+
+## Description
+
+Utilisez cette application de base pour débuter un projet. Elle contient les éléments de base pour débuter un projet.
+
+## Installation et utilisation
+
+Il y a plusieurs façons d'installer et d'utiliser cette application de base.
+
+1. Sauvegarder le dossier zippé à partir de [Github](https://github.com/js-cstj/app-base) (ou [cliquer ici](https://github.com/js-cstj/app-base/archive/refs/heads/master.zip)). Selon le projet, on peut avoir à choisir une branch spécifique avant de télécharger.
+2. Clonez le dépôt à partir de Github (voir [ici](https://github.com/js-cstj/app-base)). Faire `F1` dans _VSCode_ et taper `Git: Clone` et entrer l'adresse du dépôt.
+3. Utiliser le dépot comme modèle pour créer un nouveau dépôt. Cliquer sur le bouton `Use this template` sur Github. (Il faut avoir un compte Github pour cette méthode)
+
+## Préparation
+
+1. Au besoin, renommer le dossier selon l'application à créer.
+2. **Important** Supprimer le contenu du `#app` dans le fichier `index.html`.
+3. Adapter le `header` et le `footer` du fichier `index.html` selon les besoins.
+4. Choisir un [thème de polices](css/polices/index.html) dans le fichier `style.scss`. (Au besoin, supprimer les thèmes non utilisés du dossier `css/polices`)
+5. Modifier la méthode `main()` du fichier `src/App.js` en fonction des besoins.
---
+++ personnages.js
@@ -0,0 +1,82 @@
+export default {
+	"plante-verte": {
+		nom: "Plante-Verte",
+		img: "plante-verte.webp",
+		description: "Plante Verte est l'héroïne de la nature et de l'environnement, capable de canaliser la puissance des plantes pour protéger la planète. Son costume vert symbolise son lien profond avec la flore, et ses capacités lui permettent de manipuler la végétation pour repousser ses ennemis ou venir en aide aux plus vulnérables.",
+	},
+	"belle-maman": {
+		nom: "Belle-Maman",
+		img: "belle-maman.webp",
+		description: "Belle-Maman est une héroïne qui ne plaisante pas avec l'ordre et la justice. Avec son costume tricolore rétro en rouge, jaune et bleu, orné d'une étoile fièrement affichée sur la poitrine, elle est la gardienne des quartiers qu'elle protège à coups bien placés.",
+	},
+	"siffleux": {
+		nom: "Siffleux",
+		img: "siffleux.webp",
+		description: "Siffleux est le héros de la construction et des solutions pratiques, toujours prêt à bâtir un avenir meilleur. Vêtu d'un costume rouge et jaune flamboyant et coiffé d'un casque de chantier jaune, il est l'incarnation de la force et de la détermination. Avec une clé dans une main et une brique jaune dans l'autre, Siffleux est prêt à affronter tous les obstacles.",
+	},
+	"bob": {
+		nom: "Bob",
+		img: "bob.webp",
+		description: "Bob est le héros de la construction et de l'ingéniosité, un véritable maître des outils avec un sourire contagieux et une attitude toujours positive. Arborant une combinaison jaune éclatante et un casque assorti, il est prêt à relever tous les défis. Ses deux clés à molette sont ses armes de prédilection, symbolisant sa polyvalence et son expertise.",
+	},
+	"bobette": {
+		nom: "Bobette",
+		img: "bobette.webp",
+		description: "Bobette est la reine du bricolage et de la réparation ! Armée de ses outils géants, elle peut réparer, assembler ou démonter tout ce qui croise son chemin. Avec son casque jaune de chantier et son sourire imperturbable, elle n'hésite pas à en découdre quand il s'agit de remettre les choses en état. Son super-pouvoir ? Transformer n'importe quel outil en arme redoutable ou en gadget multifonctions.",
+	},
+	"velaire": {
+		nom: "Vélaire",
+		img: "velaire.webp",
+		description: "Dotée d'un pouvoir de vol prodigieux, Vélaire peut traverser les cieux en un éclair, sa main fermée reflétant son implacable volonté de vaincre, et sa main ouverte symbolisant l'espoir qu'elle apporte à ceux qu'elle sauve. Elle ne recule devant aucun danger et fait face à l'injustice avec une détermination infaillible.",
+	},
+	"crotteman": {
+		nom: "CrotteMan",
+		img: "crotteman.webp",
+		description: "Crotte-Man est le héros dont personne ne veut, mais que tout le monde finit par adorer ! Avec son sourire malicieux et son costume marron assorti à sa cape, il n'a pas peur de se salir les mains pour sauver la situation ! Son arme secret ? Une capacité unique à semer la confusion parmi ses ennemis grâce à ses attaques...",
+	},
+	"agent-blaze": {
+		nom: "Agent Blaze",
+		img: "agent-blaze.webp",
+		description: "Agent Blaze est une super-héroïne élégante et puissante, combinant classe et force dans un style impeccable. Avec sa longue chevelure rousse flamboyante et son costume bleu formel, elle est toujours prête à affronter n'importe quelle situation, qu'il s'agisse de réunions cruciales ou de combats épiques.",
+	},
+	"eau-dure": {
+		nom: "Eau-Dure",
+		img: "eau-dure.webp",
+		description: "Eau-Dure est un super-héros imposant et sérieux, prêt à sauver le monde avec une détermination inébranlable. Avec ses cheveux bruns et son masque mystérieux, il incarne la force tranquille et le courage face au danger. Eau-Dure est le héros que personne n'ose sous-estimer, un protecteur stoïque avec un cœur de guerrier.",
+	},
+	"capitaine-brise": {
+		nom: "Capitaine Brise",
+		img: "capitaine-brise.webp",
+		description: "<i>Capitaine Brise</i> est un super-héros léger et rapide, capable de manipuler les courants d'air avec son inséparable vire-vent. Toujours optimiste et prêt à aider, il apporte une touche d'humour et de légèreté à chaque mission. Avec son grand sourire et son allure enfantine, il est le champion des brises douces et des rafales surprises.",
+	},
+	"erudit": {
+		nom: "L'Érudit",
+		img: "erudit.webp",
+		description: "L'Érudit est un super-héros à l'intellect acéré et à la force impressionnante. Avec ses cheveux noirs en bataille et son masque distinctif, il incarne le mélange parfait de sagesse et de puissance. Revêtu d'une cape qui flotte majestueusement derrière lui, il dégage une aura de mystère. Dans ses mains, il tient un livre ancien, qu'il utilise comme une arme.",
+	},
+	"collector": {
+		nom: "Collector",
+		img: "collector.webp",
+		description: "Collector est un super-héros audacieux et astucieux, déterminé à faire régner l'ordre dans la ville. Il est facilement reconnaissable grâce à son emblème en forme d'étoile. Toujours prêt à se lancer dans l'action, il prouve que la justice se trouve dans les détails, et qu'un véritable héros est celui qui veille à ce que chacun respecte les règles du jeu.",
+	},
+	"rhino-feroce": {
+		nom: "Rhino-Féroce",
+		img: "rhino-feroce.webp",
+		description: "Rhino-Féroce est un super-héros audacieux, arborant un sourire confiant et une énergie contagieuse. Toujours prêt à charger dans la bataille avec son imposante carrure musclée, il porte fièrement une cape flottante et un masque mystérieux. Rhino-Féroce combine puissance brute et charme intrépide, un héros avec l'intensité d'un rhinocéros et le cœur d'un champion.",
+	},
+	"pro-pulsion": {
+		nom: "Pro-Pulsion",
+		img: "pro-pulsion.webp",
+		description: "Pro-Pulsion est un super-héros dynamique et plein d'énergie, prêt à s'élancer vers l'action avec une détermination inébranlable. Avec ses cheveux bruns et son sourire confiant, il dégage une aura de charisme et de force. Vêtu d'un costume saisissant avec des bottes bleues, il est toujours prêt à décoller vers de nouvelles aventures. Il est déterminé à faire régner la justice et à défendre ceux qui en ont besoin.",
+	},
+	"arachnys": {
+		nom: "Arachnys",
+		img: "arachnys.webp",
+		description: "Arachnys est une super-héroïne redoutable, vêtue d'un costume rouge et noir avec des accents jaunes qui rappellent ses origines mystérieuses liées au monde des araignées. En volant gracieusement à travers les airs grâce à sa cape, elle utilise ses capacités d'agilité extrême, de force surhumaine, et de réflexes semblables à ceux d'une araignée pour combattre le crime.",
+	},
+	"super-menton": {
+		nom: "Super-Menton",
+		img: "super-menton.webp",
+		description: "Super-Menton est un super-héros au sourire inébranlable et à la détermination à toute épreuve. Avec son impressionnant menton proéminent Super-Menton est toujours prêt à se lancer dans l'action, utilisant son incroyable force et sa résilience pour combattre le mal et protéger les innocents.",
+	},
+};
